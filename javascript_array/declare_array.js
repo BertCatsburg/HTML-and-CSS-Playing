3ea7b174193ba7e86a19f36e037d5f65f29579cd
Bert Catsburg <bert@catsburg.com>
2025-01-03 12:30:15 +0100

    Various

---
+++ javascript_array/declare_array.js
@@ -0,0 +1,4 @@
+var array1 = (1, 2, 3)
+var array2 = new Array((1, 2, 3))
+console.log(typeof array2)
+console.log(array2)
---
+++ javascript_array/string+02.js
@@ -0,0 +1,17 @@
+var a = new String('Hello');
+
+var b = "Hello";
+
+if (a === b) {
+
+    console.log("Same")
+
+
+} else {
+
+    console.log("Different")
+    console.log(typeof a)
+    console.log(typeof b)
+
+
+}
